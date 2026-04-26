7628510ec6deff9a4fc7218a7416b1d49dfda146
Md. Touhidur Rahman <touhidurrr@pm.me>
2026-04-26 15:08:26 +0600

    update ota for bun and cloudflare

--- ota/cloudflared/install.sh
+++ ota/cloudflared/install.sh
@@ -1,9 +1,12 @@
 # Add cloudflare gpg key
 sudo mkdir -p --mode=0755 /usr/share/keyrings
-curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg | sudo tee /usr/share/keyrings/cloudflare-public-v2.gpg >/dev/null
+curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg \
+  | sudo tee /usr/share/keyrings/cloudflare-public-v2.gpg >/dev/null
 
 # Add this repo to your apt repositories
-echo 'deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main' \
+  | sudo tee /etc/apt/sources.list.d/cloudflared.list
 
 # install cloudflared
-sudo apt update && sudo apt install cloudflared
+sudo apt update
+sudo apt install cloudflared
